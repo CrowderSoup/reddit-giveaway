66d79710a71fc47f9aa477d9b98accc95784a914
Aaron Crowder <aaron.crowder@gmail.com>
2016-01-11 17:58:06 -0700

    Update to improve functionality
    The script now:
    - Is case insensitive, all entries are converted to lowercase, along
      with match text
    - Duplicate entries... they won't happen
    - Track non-matching entries in a separate file for reasons

--- giveaway-bot.py
+++ giveaway-bot.py
@@ -8,6 +8,21 @@ that match a pattern into a giveaway.
 import praw
 from config import *
 
+
+def has_matching_line(csv_file_name, name):
+    matching_line = False
+
+    with open(csv_file_name, 'r') as csv_file:
+        matching_line = csv_file.read().find(comment.author.name)
+
+    return matching_line != -1
+
+def write_line(csv_file, name, permalink):
+    """
+    Write a line to a given csv
+    """
+    csv_file.write('"{0}", "{1}"\n'.format(name, permalink))
+
 r = praw.Reddit(user_agent='Reddit Raspberry Pi Giveaway (by /u/IrishLadd)')
 submission = r.get_submission(submission_id=SUBMISSION)
 
@@ -16,9 +31,15 @@ submission.replace_more_comments(limit=None, threshold=0)
 
 for comment in submission.comments:
     #print(dir(comment))
-    if(MATCH_TEXT in comment.body and comment.is_root):
-        print("Matching Entry by /u/{0}!".format(comment.author))
-        with open("entries.csv", "a") as entries:
-            entries.write('"{0}", "{1}"'.format(comment.author, comment.permalink))
+    if(MATCH_TEXT.lower() in comment.body.lower() and comment.is_root):
+        matching = has_matching_line('entries.csv', comment.author.name)
+        if(not matching):
+            print("Matching Entry by /u/{0}!".format(comment.author))
+            with open("entries.csv", "a") as entries:
+                write_line(entries, comment.author.name, comment.permalink)
     else:
-        print("Not a matching entry by /u/{0} :(".format(comment.author))
+        matching = has_matching_line('not_matching.csv', comment.author.name)
+        if(not matching):
+            print("Not a matching entry by /u/{0} :(".format(comment.author))
+            with open("not_matching.csv", 'a') as not_matching:
+                write_line(not_matching, comment.author.name, comment.permalink)
