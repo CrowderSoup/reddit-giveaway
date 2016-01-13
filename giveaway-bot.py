2f5567eddc1a711753f8d283dc176e5ff5619cf3
Aaron Crowder <aaron.crowder@gmail.com>
2016-01-13 01:00:16 -0700

    Update to select winner from list of entrants

--- giveaway-bot.py
+++ giveaway-bot.py
@@ -5,6 +5,7 @@ Simple script that will monitor comments on a given submission, entering any
 that match a pattern into a giveaway.
 """
 
+import random, linecache
 import praw
 from config import *
 
@@ -43,3 +44,17 @@ for comment in submission.comments:
             print("Not a matching entry by /u/{0} :(".format(comment.author))
             with open("not_matching.csv", 'a') as not_matching:
                 write_line(not_matching, comment.author.name, comment.permalink)
+
+entry_count = 0
+with open("entries.csv", "r") as entries:
+    entry_count = sum(1 for _ in entries) - 1
+
+winning_entry_number = random.randint(1, entry_count)
+winning_entry = linecache.getline('entries.csv', winning_entry_number)
+winning_entry_parts = winning_entry.split(',')
+winning_user = winning_entry_parts[0].strip(' "\'\t\r\n')
+winning_permalink = winning_entry_parts[1].strip(' "\'\t\r\n')
+
+print("\n\n\nAnd the winner is...\n")
+print("/u/{0}\n".format(winning_user))
+print("Permalink: {0}".format(winning_permalink))
